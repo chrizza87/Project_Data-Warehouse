17597f7cca46a501fcc42800dfdc9ca0b367d1ce
Christian Schoeppler <christian.schoeppler@mediaman.de>
2021-04-06 11:19:42 +0200

    adds doc blocks for etl.py

--- etl.py
+++ etl.py
@@ -4,18 +4,37 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Executes the copy_table_queries to copy the data of the files from s3 into the redshift db 
+
+    Parameters
+    ----------
+    cur: The psycopg2 cursor for the connection
+    conn: The psycopg2 connection
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Executes the insert_table_queries to insert the data from the staging tables to the fact and dimension tables inside redshift 
+
+    Parameters
+    ----------
+    cur: The psycopg2 cursor for the connection
+    conn: The psycopg2 connection
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Main function that reads the config file, connects to the redshift db, executes the loading and inserting methods and afterwerads closes the connection
+    """
     config = configparser.ConfigParser()
     config.read('config.cfg')
 
